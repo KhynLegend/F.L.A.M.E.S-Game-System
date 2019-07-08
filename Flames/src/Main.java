28118d3e04fe3e91d4246fe8c5ebfd65cca9b692
KhynLegend <kantoquetrue@gmail.com>
2019-07-08 12:10:17 +0800

    Second Change Wew

--- Flames/src/Main.java
+++ Flames/src/Main.java
@@ -1,9 +1,15 @@
 
+import java.awt.Dimension;
 import java.awt.Frame;
 
 public class Main extends Frame{
 
 	public Main() {
+		
+		setSize(new Dimension(500,400));
+		setLocationRelativeTo(null);
+		setTitle("FLAMES");
+		
 		
 	}
 	
